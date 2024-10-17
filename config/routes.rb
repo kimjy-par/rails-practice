73edb6d49aa76672fb0ace725b017738a25109ba
kimjy-par <kimjy.par@gmail.com>
2024-10-17 11:34:23 +0900

    Feat: add comment

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   root "articles#index"
   
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   # Defines the root path route ("/")
   # root "posts#index"
 end
